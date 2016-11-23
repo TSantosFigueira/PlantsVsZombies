fb0a7365015367a1adc59c3235851abf08cd8031
Alberto Neto <albertocorreia2011@gmail.com>
2016-11-23 17:58:55 -0400

    Adicionado Componente customizado
    Componente para calcular fator de perda monetária do jogador conforme
    mais inimigos cruzam as linhas finais.

---
+++ Assets/Editor/RemoveMoney.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveMoney : MonoBehaviour {
+
+    public int moneyLoserFactor;
+    
+    public int cashToLose
+    {
+        get { return Bounds.crossedBounds * moneyLoserFactor; }
+    }
+}
---
+++ Assets/Editor/RemoveMoney_Editor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof(RemoveMoney))]
+public class RemoveMoney_Editor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        RemoveMoney myTarget = (RemoveMoney)target;
+
+        myTarget.moneyLoserFactor = EditorGUILayout.IntField("Money Loser Factor", myTarget.moneyLoserFactor);
+        EditorGUILayout.LabelField("Level", myTarget.cashToLose.ToString());
+    }
+
+}
